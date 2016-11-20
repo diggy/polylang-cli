47fcca109e8240a7675acfeba0e7b050d2936f3c
Peter J. Herrel <peter@chilli.be>
2016-11-20 16:05:52 +0100

    package tests

--- bin/install-package-tests.sh
+++ bin/install-package-tests.sh
@@ -14,8 +14,6 @@ download() {
 
 install_wp_cli() {
 
-    mkdir -p $WP_TEST_DIR
-
     # the Behat test suite will pick up the executable found in $WP_CLI_BIN_DIR
     mkdir -p $WP_CLI_BIN_DIR
     download https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar $WP_CLI_BIN_DIR/wp
@@ -42,6 +40,7 @@ install_polylang() {
     php $WP_CLI_BIN_DIR/wp core config --dbname=wp_cli_test --dbuser=wp_cli_test --dbpass=password1
     php $WP_CLI_BIN_DIR/wp core install --url=$WP_TEST_URL --title="Polylang CLI" --admin_user=admin --admin_password=root --admin_email=wordpress@polylang-cli.dev
     php $WP_CLI_BIN_DIR/wp plugin install polylang && php $WP_CLI_BIN_DIR/wp plugin activate polylang
+    php $WP_CLI_BIN_DIR/wp core version --extra
 }
 
 install_wp_cli
