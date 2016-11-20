f119ee54e2baa198b8ab41e39e65a1c5e1f7d316
Peter J. Herrel <peter@chilli.be>
2016-11-20 13:52:08 +0100

    package tests

--- bin/install-package-tests.sh
+++ bin/install-package-tests.sh
@@ -14,29 +14,29 @@ download() {
 
 install_wp_cli() {
 
-	# the Behat test suite will pick up the executable found in $WP_CLI_BIN_DIR
-	mkdir -p $WP_CLI_BIN_DIR
-	download https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar $WP_CLI_BIN_DIR/wp
-	chmod +x $WP_CLI_BIN_DIR/wp
+    # the Behat test suite will pick up the executable found in $WP_CLI_BIN_DIR
+    mkdir -p $WP_CLI_BIN_DIR
+    download https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar $WP_CLI_BIN_DIR/wp
+    chmod +x $WP_CLI_BIN_DIR/wp
 
 }
 
 download_behat() {
 
-	cd $PACKAGE_DIR
-	download https://getcomposer.org/installer installer
-	php installer
-	php composer.phar require --dev behat/behat='~2.5'
+    cd $PACKAGE_DIR
+    download https://getcomposer.org/installer installer
+    php installer
+    php composer.phar require --dev behat/behat='~2.5'
 
 }
 
 install_db() {
-	mysql -e 'CREATE DATABASE IF NOT EXISTS wp_cli_test;' -uroot
-	mysql -e 'GRANT ALL PRIVILEGES ON wp_cli_test.* TO "wp_cli_test"@"localhost" IDENTIFIED BY "password1"' -uroot
+    mysql -e 'CREATE DATABASE IF NOT EXISTS wp_cli_test;' -uroot
+    mysql -e 'GRANT ALL PRIVILEGES ON wp_cli_test.* TO "wp_cli_test"@"localhost" IDENTIFIED BY "password1"' -uroot
 }
 
 install_polylang() {
-      wp plugin install polylang && wp plugin activate polylang
+      php wp-cli.phar plugin install polylang && php wp-cli.phar plugin activate polylang
 }
 
 install_wp_cli
