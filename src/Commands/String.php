3ef18db6567bea3c01c19c11dd6ef9620a0a894b
Peter J. Herrel <peter@chilli.be>
2017-03-08 20:59:22 +0100

    improve `wp pll string list` command

--- src/Commands/String.php
+++ src/Commands/String.php
@@ -14,29 +14,80 @@ class StringCommand extends BaseCommand {
      *
      * ## OPTIONS
      *
+     * [<language-code>]
+     * : The language code (slug) to get the string translations for. Optional.
+     *
+     * [--fields=<value>]
+     * : Limit the output to specific object fields. Valid values are: name, string, context, multiline, translations, row.
+     *
      * [--format=<format>]
      * : Accepted values: table, csv, json, count, yaml. Default: table
      *
+     * [--s=<value>]
+     * : Search for a string in `name` and `string` fields.
+     *
+     * [--orderby=<value>]
+     * : Define which column to sort.
+     *
+     * [--order=<value>]
+     * : Define the order of the results, asc or desc.
+     *
      * ## EXAMPLES
      *
-     *     $ wp pll string list
+     *     $ wp pll string list --s="WordPress site"
+     *
+     *     $ wp pll string list --order=asc --orderby=string
+     *
+     *     $ wp pll string list de --fields=string,translations
+     *
+     *     $ wp pll string list es --format=csv
      *
      * @subcommand list
      */
     public function list_( $args, $assoc_args ) {
 
-        $strings = \PLL_Admin_Strings::get_strings();
+        if ( isset( $args[0] ) && ! $this->pll->model->get_language( $args[0] ) ) {
+            $this->cli->error( sprintf( '%s is not a valid language slug.', $args[0] ) );
+        }
+
+        foreach ( array( 's', 'order', 'orderby' ) as $_g ) {
+            if ( $value = $this->cli->flag( $assoc_args, $_g ) ) {
+                $_GET[$_g] = $value;
+            }
+        }
+
+        $fields = $this->cli->flag( $assoc_args, 'fields' );
+
+        add_filter( 'pll_strings_per_page', function( $per_page ) { return PHP_INT_MAX; } );
+
+        $string_table = new \PLL_Table_String( $this->pll->model->get_languages_list() );
+
+        $string_table->prepare_items();
 
         $keys = $items = array();
 
-        foreach ( $strings as $key => $data ) {
-            $keys     = array_merge( $keys, array_keys( $data ) );
-            $obj      = (object) $data;
-            $obj->key = $key;
-            $items[]  = $obj;
+        foreach ( $string_table->items as $data ) {
+
+            $keys = array_merge( $keys, array_keys( $data ) );
+
+            if ( isset( $args[0] ) ) {
+                $data['translations'] = $data['translations'][$args[0]];
+            }
+
+            if ( $fields ) {
+                $data = array_intersect_key( $data, array_flip( explode( ',', $fields ) ) );
+            }
+
+            $items[] = (object) $data;
         }
 
-        $formatter = $this->cli->formatter( $assoc_args, array_merge( array( 'key' ), array_unique( $keys ) ) );
+        $keys = array_unique( $keys );
+
+        if ( $fields ) {
+            $keys = array_intersect_key( array_combine( $keys, $keys ), explode( ',', $fields ) );
+        }
+
+        $formatter = $this->cli->formatter( $assoc_args, $keys );
 
         $formatter->display_items( $items );
     }
